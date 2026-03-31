77be79764c6302d483e69d89f9f8e830101b9c95
Birat Lamsal <biratlamsal2063@gmail.com>
2026-03-31 08:40:49 +0545

    updated v

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+import Logo from '../../assets/logo.png'
+
+export const Header = () => {
+    return (
+        <div className='header'>
+            <img src={Logo} alt="" className='logo' />
+            <ul className='header-menu'>
+                <li>Home</li>
+                <li>Programs</li>
+                <li>Why</li>
+                <li>plans</li>
+                <li>Testimonials</li>
+            </ul>
+        </div>
+    )
+}
+
+
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'
+import { Header } from '../Header/Header'
+
+
+const Hero = () => {
+    return (
+        <div className="Hero">
+
+            <div className="left-h">
+                <Header />
+            </div>
+            <div className="right-h">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
