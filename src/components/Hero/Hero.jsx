d971de96b8e3fe58bffe5a203a2c4dffa78f6cbd
Birat Lamsal <biratlamsal2063@gmail.com>
2026-03-31 22:01:18 +0545

    31march update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,6 +9,23 @@ const Hero = () => {
 
             <div className="left-h">
                 <Header />
+
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>
+                        The Best Fitness Club In The Town
+                    </span>
+                </div>
+                <div className='hero-text'>
+                    <div>
+                        <span className='stroke-text'>Shape</span>
+                        <sapn>Your</sapn>
+                    </div>
+                    <div><span>Ideal Body</span></div>
+                    <div className='span'>
+                        In here we will help you shape and build your ideal body
+                    </div>
+                </div>
             </div>
             <div className="right-h">
 
